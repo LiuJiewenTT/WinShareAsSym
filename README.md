089698ee1d43ae74ea235bb5142b6e2138becb24
LiuJiewenTT <liuljwtt@163.com>
2025-09-08 20:04:06 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# WinShareAsSym
+
+This is a tool to create symbolic links to shared folders/files on Windows.
+
+## Usage
+
+1. Copy `create_file_sym.bat` to the folder where you want to create the symbolic link, if you want to create a symbolic link to files; or copy `create_dir_sym.bat` to the folder where you want to create the symbolic link, if you want to create a symbolic link to folders.
+2. Drag and drop the shared folder/file to the batch file, the script will automatically create the symbolic link.
+3. You can also run the batch file from the command line with the shared folder/file as an argument, for example: `create_file_sym.bat "C:\Users\John\Documents\Shared Documents\myfile.txt"`
+
---
+++ src/create_dir_sym.bat
@@ -0,0 +1,29 @@
+@echo off
+@echo Create symlinks for all directories passed in, under the directory where this batch file is located.
+@echo.
+@echo Usage: create_dir_sym.bat file1 [file2 file3...]
+@echo.
+@echo Example: create_dir_sym.bat "F:\My Documents\Learning\Python" "E:\My Music\Rock"
+@echo.
+@echo This will create two symlinks, "Python" and "Rock", in the current directory. One for "F:\My Documents\Learning\Python" and one for "E:\My Music\Rock".
+@echo.
+@echo Project Link: https://github.com/LiuJiewenTT/WinShareAsSym
+@echo.
+@echo Author: LiuJiewenTT
+@echo.
+@echo Email: ^<liuljwtt@163.com^>
+@echo.
+@echo Version: 1.0
+@echo.
+@echo License: MIT License ^(Copyright ^(c^) 2025 LiuJiewenTT^)
+@echo.
+@echo ---
+
+if "%~1"=="" goto:eof
+
+:loop
+set source_file=%~1
+set source_filename=%~nx1
+mklink /D "%~dp0%source_filename%" "%source_file%"
+shift /1
+if not "%~1"=="" goto loop
