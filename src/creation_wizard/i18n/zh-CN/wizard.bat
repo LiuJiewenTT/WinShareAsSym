da65314d414af4b4309f403892cb2b75f54ad33d
LiuJiewenTT <liuljwtt@163.com>
2025-09-27 18:09:19 +0800

    改用choice实现更好的体验

--- src/creation_wizard/i18n/zh-CN/wizard.bat
+++ src/creation_wizard/i18n/zh-CN/wizard.bat
@@ -4,31 +4,22 @@ if "%~1" EQU "" (
     set target_dir=%~1
 )
 echo 目标目录: %target_dir% 
-
-:sym_type_select_loop
-
+echo.
 echo 1. 文件符号链接 
 echo 2. 目录符号链接 
 echo 3. 文件硬链接 
 echo 4. 目录软联结 
 echo.
-set /p sym_type_no="请输入数字选择符号链接类型："
+choice /c 1234 /n /m "请选择符号链接类型："
 
-if "%sym_type_no%" EQU "1" (
-    copy "%~dp0create_file_sym.bat" "%target_dir%"
-) else if "%sym_type_no%" EQU "2" (
-    copy "%~dp0create_dir_sym.bat" "%target_dir%"
-) else if "%sym_type_no%" EQU "3" (
-    copy "%~dp0create_file_sym_h.bat" "%target_dir%"
-) else if "%sym_type_no%" EQU "4" (
-    copy "%~dp0create_dir_sym_j.bat" "%target_dir%"
-) else if "%sym_type_no%" EQU "" (
-    echo 未输入任何内容，程序退出。
-    goto :eof 
-) else (
-    echo 无效的选项，请重新输入正确的数字。 
-    echo.
-    goto :sym_type_select_loop
-)
+if ERRORLEVEL 4 (
+    copy "%~dp0..\create_dir_sym_j.bat" "%target_dir%"
+) else if ERRORLEVEL 3 (
+    copy "%~dp0..\create_file_sym_h.bat" "%target_dir%"
+) else if ERRORLEVEL 2 (
+    copy "%~dp0..\create_dir_sym.bat" "%target_dir%"
+) else if ERRORLEVEL 1 (
+    copy "%~dp0..\create_file_sym.bat" "%target_dir%"
+) 
 
 
