fb720faaa15a8e143922186ad65dad7d9f47f595
LiuJiewenTT <liuljwtt@163.com>
2026-02-11 16:48:46 +0800

    添加部分string工具

---
+++ src/extensions/refactor_link_root/utils/str/is_blank.bat
@@ -0,0 +1,24 @@
+@setlocal
+@echo off
+@REM retv: (None)
+@REM exit code: 0 if the string is blank, 1 otherwise.
+@REM para1: (str) the string to be checked
+
+
+set retv=
+set para1=%~1
+for /f "" %%i in ("%para1%") do (
+    set retv=%%i
+    @REM not equals to blank and tab
+    if /I "%%i" NEQ " " if /I "%%i" NEQ "	" (
+        goto :break
+    )
+)
+:break
+if not defined retv (
+    exit /b 0
+) else (
+    if /I "%flag_wasa_debug_mode%" EQU "true" (echo [DEBUG]: not blank)
+    exit /b 1
+)
+@endlocal
---
+++ src/extensions/refactor_link_root/utils/str/lstrip.bat
@@ -0,0 +1,53 @@
+@setlocal
+@echo off
+@REM This is a batch script to remove the first character of a string.
+@REM Usage: remove_first_char.bat [first_char] [string]
+@REM retv: (str) The result string.
+@REM para1: (str) The first character to remove.
+@REM para2: (str) The count of characters to remove.
+@REM para3: (str) The string to remove characters from.
+
+
+set retv=
+set "para1=%~1"
+set "para2=%~2"
+set "para3=%~3"
+
+set /a cnt=0
+
+if /I "%para3%" EQU "" (
+    goto :end
+) 
+
+set /a "max_cnt=%para2%"
+
+if /I "%flag_wasa_debug_mode%" EQU "true" (echo [DEBUG] max_cnt=【%max_cnt%】 )
+
+:loop
+
+if /I "%flag_wasa_debug_mode%" EQU "true" (echo [DEBUG] para3=【%para3%】 )
+
+if /I "%flag_wasa_debug_mode%" EQU "true" (echo [DEBUG] first_char=【%para3:~0,1%】 )
+
+if "%para3:~0,1%" EQU "%para1%" (
+    set "para3=%para3:~1%"
+    set /a cnt+=1
+) else (
+    goto :end
+)
+
+if /I "%flag_wasa_debug_mode%" EQU "true" (echo [DEBUG] cnt=【%cnt%】 )
+
+if /I "%max_cnt%" GTR "0" if /I "%cnt%" GEQ "%max_cnt%" (
+    if /I "%flag_wasa_debug_mode%" EQU "true" (echo [DEBUG] cnt=【%cnt%】 hits max_cnt=【%max_cnt%】 )
+    goto :end
+)
+
+if /I "%para3%" NEQ "" (
+    goto :loop
+)
+
+:end
+set "retv=%para3%" 
+if /I "%flag_wasa_debug_mode%" EQU "true" (echo [DEBUG] retv=【%retv%】 )
+@endlocal && set "retv=%retv%"
