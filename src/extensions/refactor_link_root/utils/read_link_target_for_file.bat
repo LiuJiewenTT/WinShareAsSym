7f0ccf8447758689d10f5948c3eacbc2574a2de6
LiuJiewenTT <liuljwtt@163.com>
2026-02-11 15:34:35 +0800

    尝试兼容方括号-失败
    被findstr偏移量-1肘击了，方案失败

--- src/extensions/refactor_link_root/utils/read_link_target_for_file.bat
+++ src/extensions/refactor_link_root/utils/read_link_target_for_file.bat
@@ -1,8 +1,41 @@
 set retv=
-@for /f "usebackq eol= tokens=2 delims=[]" %%i in (`dir /A:-DL "%~1" ^| findstr /C:" %~nx1 "`) do @(
+set token_3=
+set line=
+set str_length=
+set offset=
+@for /f "usebackq eol= delims=" %%i in (`dir /A:-DL "%~1" ^| findstr /L /C:" %~nx1 "`) do @(
+    set line=%%i
+)
+echo "line=%line%"
+@for /f "usebackq tokens=2,* delims=[]" %%i in (`echo "%line%"`) do @(
     echo %%i
     set retv=%%i
+    set token_3=%%j
 )
+@REM remove tailing " sign. 
+@ if defined token_3 (
+    set token_3=%token_3:~,-1%
+)
+@ if not defined token_3 (
+    goto:end
+)
+echo "token_3=%token_3%"
+
+set /a offset=0
+echo "%line%" | findstr /L /O /C:" %~nx1 "
+@for /f "usebackq tokens=1 delims=:" %%i in (`echo "%line%" ^| findstr /L /O /C:" %~nx1 "`) do @(
+    echo %%i
+    set /a offset=%%i-1
+)
+echo "offset=%offset%"
+@for /f "usebackq tokens=1 delims=:" %%i in (`echo "%~nx1%:" ^| findstr /L /O /C:":"`) do @(
+    set /a offset=%offset%+%%i-1
+)
+echo "offset=%offset%"
+set "retv=%line:~%offset%,-1%"
+echo %retv%
+
+:end
 if not defined retv (
     exit /b 1
 )
